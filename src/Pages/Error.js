30620b45ea8195c414a9fac8097a1d0119dae4b4
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2022-09-29 12:26:33 +0530

    some more changes in error page

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -4,7 +4,7 @@ const Error = () => {
   return (
     <div className="flex justify-center items-center min-h-screen w-screen">
       <h1 className="text-4xl font-medium text-red-700">
-        Error in find this page madharchode
+        Error in find this page madharchode saala
       </h1>
     </div>
   );
